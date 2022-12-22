50fd3640be9acdeddd62af2cec8162a752a63d00
JohnnyLoeve <Sion_keven@hotmail.com>
2022-12-22 10:36:06 +0100

    Innit

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,54 @@
 public class Book {
+
+    int BookId;
+    String Title;
+    String Author;
+    int Price;
+
+    //Constructor for book.
+    public Book(int bookId, String title, String author, int price) {
+        BookId = bookId;
+        Title = title;
+        Author = author;
+        Price = price;
+    }
+
+    //Prints the details of book.
+    public void printBook(){
+        System.out.println("Book ID: " + this.BookId + ". Title: " + this.Title + ". Author: " + this.Author + ". Price: " + this.Price);
+    }
+
+
+    //Getters and setters for the variables of Book.
+    public int getBookId() {
+        return BookId;
+    }
+
+    public void setBookId(int bookId) {
+        BookId = bookId;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String author) {
+        Author = author;
+    }
+
+    public int getPrice() {
+        return Price;
+    }
+
+    public void setPrice(int price) {
+        Price = price;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+
+        Book book1 = new Book(12, "The oldest book", "Old book writer McOld", 399);
+        Book book2 = new Book(22, "The newest book", "New book writer McNew", 499);
+        Book book3 = new Book(32, "The medium old book", "Medium old book writer McMedium", 449);
+        System.out.println("------------------");
+        System.out.println("Welcome to the BookStore! ");
+        System.out.println("What would you like to do? ");
+        System.out.println("------------------");
+        System.out.println("\n");
+        System.out.println("\t1. Browse the collection");
+        System.out.println("\t2. Add book to your cart.");
+        System.out.println("\t3. Remove book from cart");
+        System.out.println("\t4. Show cart");
+        System.out.print("\tPlease enter your choice: ");
+
+       int choice = scanner.nextInt();
+        scanner.nextLine();
+        switch (choice){
+            case 1:
+                book1.printBook();
+                book2.printBook();
+                book3.printBook();
+                break;
+            case 2:
+
+            case 3:
+
+            case 4:
+
+
+        }
     }
 }
