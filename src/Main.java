9d67b6c5d90844115875f2b1f027f8f3d04c300a
Hauxy17 <ella.costello@yahoo.com>
2022-12-22 13:17:57 +0100

    Created Customer.java Created BookStore.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,3 @@
 public class Main {
-    public static void main(String[] args) {
-        Customer customer = new Customer("Ella", "Costello", "ella.costello@yahoo.com", 53314621, 1);
-        System.out.println(customer);
 
-
-
-        BookStore.createCustomer();
-        System.out.println(BookStore.customers.toString().replace('[', ' ').replace(']', ' '));
-    }
 }
