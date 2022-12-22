95f43a5bae3ba14af24b1e1312d9e2065112226f
Hauxy17 <ella.costello@yahoo.com>
2022-12-22 12:56:09 +0100

    Created Customer.java Created BookStore.java

--- src/BookStore.java
+++ src/BookStore.java
@@ -1,2 +1,38 @@
+import java.util.*;
+
 public class BookStore {
+    static List<Book> books = new ArrayList<>();
+    static List<Customer> customers = new ArrayList<>();
+    static List<Cart> carts = new ArrayList<>();
+
+
+    public static void createCustomer() {
+        Scanner sc = new Scanner(System.in);
+        int customerId = 1;
+
+        if (customers.size() > 0) {
+            customerId = customers.get(customers.size()-1).getCustomerId() + 1;
+        }
+
+        System.out.println("---Create new customer---");
+        System.out.print("First name: ");
+        String firstName = sc.nextLine();
+        System.out.print("Last name: ");
+        String lastName = sc.nextLine();
+        System.out.print("E-mail: ");
+        String email = sc.nextLine();
+        System.out.print("Phone Number: ");
+        int number = sc.nextInt();
+
+        customers.add(new Customer(firstName, lastName, email, number, customerId));
+
+    }
+
+    public static void addNewCart() {
+
+    }
+
+    public static void editCart() {
+
+    }
 }
--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,66 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer {
+    List<Cart> customerCarts = new ArrayList<>();
+
+    String firstName;
+    String lastName;
+    String email;
+    int phone;
+    int customerId;
+
+    public Customer(String firstName, String lastName, String email, int phone, int customerId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String toString() {
+        return  "----------------------------------------\nCustomer:\t\t|\t\t" + firstName + " " + lastName + "\nEmail:\t\t\t|\t\t" + email.toLowerCase() + "\nPhone\t\t\t|\t\t" + phone + "\nCustomer ID:\t|\t\t" + customerId + "\n----------------------------------------";
+
+    }
 }
+
